74a63bff0b397184a15971f5d481d39b6f148257
openshiftio-launchpad <obsidian-leadership@redhat.com>
2019-02-19 10:11:38 +0000

    Initial commit

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into fruit (id, name, stock) values (1, 'Apple', 10);
+insert into fruit (id, name, stock) values (2, 'Orange', 10);
+insert into fruit (id, name, stock) values (3, 'Pear', 10);
